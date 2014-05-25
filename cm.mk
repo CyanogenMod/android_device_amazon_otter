da1e0c1712a55318c9cc0f040bcd46a4a13c493e
Joshua <lj50036@users.noreply.github.com>
2014-05-25 08:11:33 -0500

    making it ours

--- cm.mk
+++ cm.mk
@@ -16,7 +16,7 @@
 $(call inherit-product, device/amazon/otter/full_otter.mk)
 $(call inherit-product, device/amazon/otter-common/cm.mk)
 
-PRODUCT_NAME := cm_otter
+PRODUCT_NAME := crombi_otter
 PRODUCT_DEVICE := otter
 PRODUCT_MODEL := Amazon Kindle Fire
 PRODUCT_RELEASE_NAME := KFire
