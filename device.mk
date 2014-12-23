b962544d17f2512318e387aacc36bd70a0e2c905
Hashcode <hashcode0f@gmail.com>
2014-12-23 13:11:10 -0800

    otter: remove legacy mountd.conf file
    Change-Id: Id6950c60be391d0abdf602d61406fbd4bf81bfa7

--- device.mk
+++ device.mk
@@ -42,7 +42,6 @@ PRODUCT_COPY_FILES += \
 
 # Prebuilt system/etc
 PRODUCT_COPY_FILES += \
-    $(DEVICE_FOLDER)/prebuilt/etc/mountd.conf:/system/etc/mountd.conf \
     $(DEVICE_FOLDER)/prebuilt/etc/firmware/ducati-m3.512MB.bin:/system/etc/firmware/ducati-m3.512MB.bin
 
 # Prebuilt system/lib
